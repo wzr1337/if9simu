832f2a68af1a60a02471877c46b040abd1d112a8
wzr1337 <bartsch98@gmail.com>
2019-11-27 08:43:34 +0000

    test(auth): add more tests

--- src/ifas/auth.spec.ts
+++ src/ifas/auth.spec.ts
@@ -5,33 +5,98 @@ import userDB from "../config/users.json";
 import auth = require("./auth");
 
 // Basic Setup
+let options;
+let optionsWrongPassword;
 
-const options = {
-  body: {
-    grant_type : "password",
-    password : userDB["ansgerl@gmail.com"].password,
-    username : "ansgerl@gmail.com",
-  },
-  headers: {
-    "authorization" : "Basic YXM6YXNwYXNz",
-    "connection"  : "close",
-    "content-type" : "application/json",
-    "x-Device-Id" : userDB["ansgerl@gmail.com"].deviceID,
-  },
-};
+beforeEach(() => {
+  options = {
+    body: {
+      grant_type : "password",
+      password : userDB["ansgerl@gmail.com"].password,
+      username : "ansgerl@gmail.com",
+    },
+    headers: {
+      "authorization" : "Basic YXM6YXNwYXNz",
+      "connection"  : "close",
+      "content-type" : "application/json",
+      "x-Device-Id" : userDB["ansgerl@gmail.com"].deviceID,
+    },
+  };
 
-const optionsWrongPassword = {
-  body: {
-    grant_type : "password",
-    password : "wrong_password",
-    username : "ansgerl@gmail.com",
-  },
-  ...options.headers,
-};
+  optionsWrongPassword = {
+    body: {
+      grant_type : "password",
+      password : "wrong_password",
+      username : "ansgerl@gmail.com",
+    },
+    ...options.headers,
+  };
+});
 
 describe("TEST auth", () => {
 
-  test("TEST /ifas/jlr/tokens", () => {
+  it("should initialize", () => {
+    auth.init();
+  });
+
+  it("should know VINS", () => {
+    auth.init();
+    expect(auth.getKnownVINS().length).toBeGreaterThan(0);
+  });
+
+  it("should get username by vin", () => {
+    auth.init();
+    expect(auth.getUserNameByVIN(userDB["ansgerl@gmail.com"].vehicles[0].vin)).toBe("ansgerl@gmail.com");
+  });
+
+  it("should get username by userId", () => {
+    auth.init();
+    expect(auth.getUserNameById(userDB["ansgerl@gmail.com"].userId)).toBe("ansgerl@gmail.com");
+  });
+
+  it("should get userId by vin", () => {
+    auth.init();
+    expect(auth.getUserIdByVIN(userDB["ansgerl@gmail.com"].vehicles[0].vin)).toBe(userDB["ansgerl@gmail.com"].userId);
+  });
+
+  it("should get vehicles by userId", () => {
+    auth.init();
+    expect(auth.getVehiclesByUserId(userDB["ansgerl@gmail.com"].userId).length).toBeGreaterThan(0);
+  });
+
+  it("should check PIN", () => {
+    auth.init();
+    expect(auth.checkPIN(userDB["ansgerl@gmail.com"].userId, userDB["ansgerl@gmail.com"].pin)).toBeTruthy();
+    expect(auth.checkPIN(userDB["ansgerl@gmail.com"].userId, "")).toBeFalsy();
+  });
+
+  it("should reject queries to /ifas/jlr/tokens with missing content type header", () => {
+    const opts = options;
+    opts.headers["content-type"] = "";
+
+    const req = new MockExpressRequest(opts);
+    const res = new MockExpressResponse();
+
+    const spySendStatus = jest.spyOn(res, "sendStatus");
+    auth.tokens(req, res);
+    expect(spySendStatus).toHaveBeenCalled();
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should reject queries to /ifas/jlr/tokens with missing wrong grant type", () => {
+    const opts = options;
+    opts.body.grant_type = "thisisallwrong@$hyte";
+
+    const req = new MockExpressRequest(opts);
+    const res = new MockExpressResponse();
+
+    const spySendStatus = jest.spyOn(res, "sendStatus");
+    auth.tokens(req, res);
+    expect(spySendStatus).toHaveBeenCalled();
+    expect(res.statusCode).toBe(403);
+  });
+
+  it("should respond properly to /ifas/jlr/tokens queries", () => {
     const req = new MockExpressRequest(options);
     const res = new MockExpressResponse();
 
